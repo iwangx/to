5f4072d1287281f973b1b528d7511c043dfa504e
iwangx <451749469@qq.com>
2016-06-29 17:59:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express=require("express");
 var path=require("path");
 var routes = require('./route');
 var app = express();
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'html'));
 app.set('view engine', 'ejs');
 var env =process.argv.slice(2)[1];
